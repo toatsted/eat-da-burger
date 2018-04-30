51cce18f3c2df2e6034418a82a751e39022f80df
Sam <samueljacksonhicks@gmail.com>
2018-04-30 14:24:12 -0500

    C:/Program Files/Git/ path works

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,5 +1,13 @@
-const burgerjs = require("../models/burger.js");
+// const burgerjs = require("../models/burger.js");
 
 module.exports = function(app){
-	
+	app.get("/", (req, res) => {
+		req.db.query(`
+				SELECT * FROM burgers
+			`)
+			.then(data => {
+				res.render("index", {burgers: data})
+			})
+			.catch(err => console.log(err))
+	})
 }
--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.set("view engine", "handlebars");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// mysql connectionz
+require("./config/connection.js")(app);
+
 require("./controllers/burgers_controller.js")(app);
 
 app.listen(PORT, () => console.log(`app is listening on https://localhost:${PORT}`));
